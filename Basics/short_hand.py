0dc347f0c8f636d3d4e34da18c328057599c4be0
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-06-20 17:29:18 +0530

    done short hand

---
+++ Basics/short_hand.py
@@ -0,0 +1,25 @@
+# short hand
+# statement if condtion else else-statement
+# statement if condtion else nested-if-statement else  nested-else-statement
+a = 330
+b = 3303
+
+print ("A") if a > b else print("=") if a == b else print("b") # b 
+"""
+if a > b :
+    print ("A")
+else:
+    if a == b:
+        print("=")
+    else:
+    print("b")
+"""
+c = 9 if a < b else 0  
+
+print(c) #9
+
+
+#it is not recommended for large code with multiple if - else 
+
+
+
