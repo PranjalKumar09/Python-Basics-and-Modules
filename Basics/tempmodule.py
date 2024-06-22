70dacb652552f48bc49d7f84dda1d40872e3da87
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-06-22 10:31:42 +0530

    done temp module

---
+++ Basics/tempmodule.py
@@ -0,0 +1,9 @@
+def welcome():
+    print("You are welcome")
+
+
+if __name__ == "__main__":
+    welcome()
+print(__name__) #__main__
+#from where it running is meaning of __name__
+#but in other imported file it will print tempmodule 
