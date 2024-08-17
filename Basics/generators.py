344968f2240e108e4f3c23f173e695d2772cf025
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-08-17 19:34:27 +0530

    done  the generators

---
+++ Basics/generators.py
@@ -0,0 +1,23 @@
+def my_generator():
+    for i in range(10):
+        yield i
+gen = my_generator()
+print(next(gen)) # 0
+print(next(gen)) # 1
+print(next(gen)) # 2
+print(next(gen)) # 3
+print(next(gen)) # 4
+print(next(gen)) # 5
+print(next(gen)) # 6
+print(next(gen)) # 7
+print(next(gen)) # 8
+print(next(gen)) # 9
+print(next(gen)) # ERROR 
+
+
+
+
+
+
+        
+        
