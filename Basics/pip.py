bb4c726cc7c655006cd83ee56f5d9d640b5e34f1
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-08-25 00:22:40 +0530

    done the pip

---
+++ Basics/pip.py
@@ -0,0 +1,34 @@
+"""
+PEP is a document that provides guidelines and best practice on how to write a python code .
+it was written by guidp von rossum AND berry warsaw pyhton 
+Python pip 
+pip = package manager for packages and modules from python packages index
+
+open cmd
+type pip
+
+help:                       pip
+check:                      pip --version
+update:                     pip install --upgrade pip
+installed packages:         pip list
+check outdated pakcages:    pip list --outdated
+install package:            pip instal "package name"
+
+install update particular package pip install my_package -U 
+
+search python library in https://pypi.org/ 
+
+
+Fun fact
+Zen of pyhton , by tin peters
+
+open command shell (in windows)
+
+type python 
+then type import this
+then poem appears
+
+
+
+To unistall all the packages -> pip freeze | xargs pip uninstall -ye
+"""
