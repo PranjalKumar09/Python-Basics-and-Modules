aa59910301a9b4b63a8c3e3027aff1f4bfe9dd01
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-08-25 22:30:20 +0530

    done the cmd sample

---
+++ Basics/cmd_run_sample.py
@@ -0,0 +1,18 @@
+"""
+Steps to run a file command promt 
+
+copy file path let us example this is file C:\Users\PRANJAL\Desktop\CODES\Python\cmd_run_sample.py
+
+now in cmd paste address folder location this without colon "cd C:\Users\PRANJAL\Desktop\CODES\Python"
+now type "python"
+now type file name "cmd_run_sample.py"
+your file will run now offcourse all adove without colon marks ()")
+
+
+"""
+
+
+
+print("Namaste duniya")
+name = input(" Enter your name: ")
+print("Namaskar , ", name)
